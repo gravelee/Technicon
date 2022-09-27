6bae787e1376143ebc4007ffa5bc8162f612360d
gravelee <grave2lee@gmail.com>
2022-09-27 21:56:31 +0300

    Forgot to add the Record class to the commit. Sorry!

---
+++ src/main/java/com/technicon/renovationcontractorapp/domain/Record.java
@@ -0,0 +1,12 @@
+package com.technicon.renovationcontractorapp.domain;
+
+/**
+ * 	We define this class so User, Property &
+ *  PropertyRepair can be used interchangeably
+ *  in the generic methods of Repository.
+ * 
+ * 	@author Grproth
+ */
+public abstract class Record {
+
+}
